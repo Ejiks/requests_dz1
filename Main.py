c1a65883ef113178663c854eb6893520366e8dac
Ejiks <ivanbirulia@gmail.com>
2022-02-27 23:22:30 +0300

    Final fix

---
+++ Main.py
@@ -0,0 +1,20 @@
+import requests
+
+TOKEN = "2619421814940190"
+
+def hero_requests(hero_list):
+    intel_dic = {}
+    for name in hero_list:
+        url = f"https://superheroapi.com/api/{TOKEN}/search/{name}"
+        response = requests.get(url)
+        h_list = response.json().get('results')
+        intel_val = h_list[0].get("powerstats")
+        intel_dic.update({int(intel_val.get("intelligence")):name})
+        max_int = max(list(intel_dic.keys()))
+    hero_name = intel_dic.get(max_int)
+    print(f"Самый умный герой {hero_name}.\nЕго уровень интеллекта {max_int}.")
+    
+
+    
+hero_list = ["Hulk", "Captain America", "Thanos"]
+hero_requests(hero_list)
